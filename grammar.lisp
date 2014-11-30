53810d4d1ce1d3595285fcc2dfaca5dbb56e14da
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2014-11-29 23:26:53 -0800

    Add more AST rules and tests

--- grammar.lisp
+++ grammar.lisp
@@ -36,7 +36,7 @@
 
 (defrule document (* block-top)
   (:lambda (r)
-    `(:document ,r)))
+    `(:document ,@r)))
 
 (defrule block-top (or block-blank
                        block-comment
@@ -53,50 +53,74 @@
 
 (defrule block-blank line-blank)
 
-(defrule block-comment-sep (and HASH HASH HASH EOL))
+(defrule block-comment-sep (and HASH HASH HASH EOL)
+  (:constant nil))
 
 (defrule block-comment (and block-comment-sep
                             (* (and (! block-comment-sep)
                                     (* (and (! EOL) ANY)) EOL))
                             block-comment-sep
                             (? line-blank))
-  (:destructure (sep1 comment sep2 &rest rest)
-    (declare (ignore sep1 sep2 rest))
-    `(:comment ,(text comment))))
+  (:lambda (x)
+    `(:block-comment ,(text (second x)))))
 
 (defrule line-comment (and HASH (? SPACE) (* (and (! EOL) ANY)) EOL
-                           (? line-blank)))
+                           (? line-blank))
+  (:lambda (x)
+    `(:comment ,(text (third x)))))
 
 (defrule block-rule (and "----" EOL
-                         (? line-blank)))
+                         (? line-blank))
+  (:constant :block-rule))
 
-(defrule block-meta-beg (and "---" EOL))
-(defrule block-meta-end (and "..." EOL))
+(defrule block-meta-beg (and "---" EOL)
+  (:constant nil))
+(defrule block-meta-end (and "..." EOL)
+  (:constant nil))
 
 (defrule block-meta (and block-meta-beg
                          (* (and (! block-meta-end)
                                  (* (and (! EOL) ALL)) EOL))
-                         block-meta-end))
+                         block-meta-end)
+  (:lambda (x)
+    `(:block-meta ,(text (second x)))))
 
 (defrule block-head (and (or "====" "===" "==" "=") (+ SPACE)
                          (or block-head-plain
                              block-head-multi
                              block-head-marker)
-                         (? line-blank)))
+                         (? line-blank))
+  (:lambda (x)
+    (let ((head (case (length (first x))
+                  (1 :head-1)
+                  (2 :head-2)
+                  (3 :head-3)
+                  (4 :head-4))))
+      `(,head ,(third x)))))
 
 (defrule block-head-plain (and (+ (and (! SPACE) ANY))
-                               (+ SPACE) (+ EQUAL) EOL))
+                               (+ SPACE) (+ EQUAL) EOL)
+  (:lambda (x)
+    (text (first x))))
 
+;;; FIXME Break down this rule to multiple sub-rules
 (defrule block-head-multi (and (+ ANY) EOL
-                               (* (and (not WS) (* ANY) EOL))
-                               (not WS) (* (and (! block-head-multi-end) ANY))
-                               block-head-multi-end))
+                               (* (and (not WS)
+                                       (* (and (! block-head-multi-end) ANY))
+                                       EOL))
+                               (not WS)
+                               (* (and (! block-head-multi-end) ANY))
+                               block-head-multi-end)
+  (:text t))
 
-(defrule block-head-multi-end (and (+ SPACE) (+ EQUAL) EOL))
+(defrule block-head-multi-end (and (+ SPACE) (+ EQUAL) EOL)
+  (:constant nil))
 
 (defrule block-head-marker (and (+ ANY) EOL
-                                (* (and (not WS) (* ANY) EOL))
-                                (& (or marker-block-start EOL EOS))))
+                                (* (and (! marker-block-start)
+                                        (not WS) (* ANY) EOL))
+                                (& (or marker-block-start EOL EOS)))
+  (:text t))
 
 (defrule block-code "///")
 
